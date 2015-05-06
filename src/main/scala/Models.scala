0c4107331e8206b5ba1f1b44a7538421ed36d59e
Doug Beasley <beasley.doug@gmail.com>
2015-05-05 17:05:38 -0700

    comment clean up

--- src/main/scala/Models.scala
+++ src/main/scala/Models.scala
@@ -8,15 +8,10 @@ import play.api.libs.json._
 case class Image(id : String, url : String, cdn : String)
 case class Stats(reported : Int, favorites : Int, found : Int)
 case class Post(id: String, image: Image, stats: Stats)
-// object Post {
-//   implicit val reader: BSONDocumentReader[Post] = Macros.reader[Post]
-
-// }
 
 object JsonFormats {
 	import play.api.libs.json.Json
 	import play.api.data._
-//	import play.api.data.Formats._
 
 	implicit val imageFormat = Json.format[Image]
 	implicit val statsFormat = Json.format[Stats]
